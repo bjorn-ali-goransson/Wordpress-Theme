0a04f6b08842e1cb1417e47b639f4c0a34830795
Björn Ali Göransson <bornemix@gmail.com>
2015-09-05 18:38:59 +0200

    Break up function "get image"

--- functions.php
+++ functions.php
@@ -1035,6 +1035,15 @@ add_action("after_switch_theme", function(){
 
 function get_image_from_meta($post_id, $meta_key, $size = 'full'){
   $image_id = get_post_meta($post_id, $meta_key, TRUE);
+
+  return get_image($image_id, $size);
+}
+
+
+
+/* GET IMAGE */
+
+function get_image($image_id, $size = 'full'){
   $image = wp_get_attachment_image_src($image_id, $size);
 
   return (object)array(
