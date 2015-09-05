8a45d41d9378f6b654a552f1fe753b30261ea5e2
Björn Ali Göransson <bornemix@gmail.com>
2015-09-05 12:14:03 +0200

    Trash default content, first implementation

--- functions.php
+++ functions.php
@@ -990,6 +990,15 @@ function my_option($key){
 
 
 
+/* TRASH DEFAULT CONTENT */
+
+add_action("after_switch_theme", function(){
+  wp_trash_post(1); // Sample post
+  wp_trash_post(2); // Sample page
+});
+
+
+
 /* OTHER */
 
 add_theme_support('automatic-feed-links');
