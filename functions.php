1c3ce4c88dc83033ed06ccf5c3e09aa0c4ea7a0f
Björn Ali Göransson <bornemix@gmail.com>
2015-09-22 00:59:49 +0200

    Add tools page

--- functions.php
+++ functions.php
@@ -8,6 +8,32 @@ require DIRNAME(__FILE__) . '/application.php';
 
 
 
+/* ADD TOOLS PAGE */
+
+function add_my_tools_page($title, $slug, $path){
+  if(!isset($GLOBALS['my-tools-pages'])){
+    $GLOBALS['my-tools-pages'] = array();
+  }
+
+  $GLOBALS['my-tools-pages'][] = (object)array(
+    'title' => $title,
+    'slug' => $slug,
+    'path' => $path,
+  );
+}
+
+add_action('admin_menu', function(){
+  if(!isset($GLOBALS['my-tools-pages'])){
+    return;
+  }
+  
+  foreach($GLOBALS['my-tools-pages'] as $object){
+	  add_management_page($object->title, $object->title, "manage_options", $object->slug, create_function('', 'require "' . addslashes(dirname(__FILE__)) . '/' . $object->path . '";'));
+  }
+});
+
+
+
 /* ADD ROUTE */
 
 function add_my_route($url, $arg2){
