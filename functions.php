8ae961d45258d76971dc01bc330554aa685ccad3
Björn Ali Göransson <bornemix@gmail.com>
2015-09-22 00:59:32 +0200

    Move widget base class down a bit

--- functions.php
+++ functions.php
@@ -2,48 +2,6 @@
 
 
 
-/* WIDGET BASE CLASS */
-
-require dirname(__FILE__) . "/widget.class.php";
-
-add_action('admin_head', function() {
-  ?>
-  <script>
-    (function ($) {
-      $(document).on("click", 'input.choose-image', function (event) {
-        var button = $(this);
-
-        wp.media.editor.send.attachment = function (props, attachment) {
-          button.parent().siblings("input[type=\"hidden\"]").val(attachment.id);
-          button.parent().siblings("span.my-image").html('<img src="' + attachment.sizes.thumbnail.url + '" />');
-        }
-
-        wp.media.editor.open(this);
-        event.preventDefault();
-      });
-
-      // $('div.widgets-sortables').bind('sortstop', activate);
-
-    })(jQuery);
-  </script>
-  <?php
-});
-
-add_action('admin_enqueue_scripts', function($hook) {
-  if($hook == "widgets.php"){
-    wp_enqueue_media();
-  }
-});
-
-add_action('widgets_init', function() {
-  foreach (get_declared_classes() as $class) {
-    if (is_subclass_of($class, "My_Widget"))
-      register_widget( $class );
-  }
-});
-
-
-
 /* APPLICATION LOGIC */
 
 require DIRNAME(__FILE__) . '/application.php';
@@ -251,6 +209,48 @@ function get_my_script_or_style_object($name, $arg2 = NULL, $arg3 = NULL){
     'version' => strpos($_SERVER["HTTP_HOST"], 'localhost') === FALSE ? @filemtime(dirname(__FILE__) . $path) : NULL,
   );
 }
+
+
+
+/* WIDGET BASE CLASS */
+
+require dirname(__FILE__) . "/widget.class.php";
+
+add_action('admin_head', function() {
+  ?>
+  <script>
+    (function ($) {
+      $(document).on("click", 'input.choose-image', function (event) {
+        var button = $(this);
+
+        wp.media.editor.send.attachment = function (props, attachment) {
+          button.parent().siblings("input[type=\"hidden\"]").val(attachment.id);
+          button.parent().siblings("span.my-image").html('<img src="' + attachment.sizes.thumbnail.url + '" />');
+        }
+
+        wp.media.editor.open(this);
+        event.preventDefault();
+      });
+
+      // $('div.widgets-sortables').bind('sortstop', activate);
+
+    })(jQuery);
+  </script>
+  <?php
+});
+
+add_action('admin_enqueue_scripts', function($hook) {
+  if($hook == "widgets.php"){
+    wp_enqueue_media();
+  }
+});
+
+add_action('widgets_init', function() {
+  foreach (get_declared_classes() as $class) {
+    if (is_subclass_of($class, "My_Widget"))
+      register_widget( $class );
+  }
+});
 
 
 
