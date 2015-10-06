1db349f2bc2a57aacbdb320b3f09c09998be7a0c
Björn Ali Göransson <bornemix@gmail.com>
2015-10-06 09:38:57 +0200

    Move pagination into function module

--- functions.php
+++ functions.php
@@ -59,40 +59,6 @@ add_filter('embed_oembed_html', function($html, $url, $attr, $post_ID) {
 
 
 
-/* PAGINATION */
-
-function my_pagination(){
-  global $wp_query;
-
-  $big = 999999999; // need an unlikely integer
-
-  $links = paginate_links( array(
-	  'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-	  'format' => '?paged=%#%',
-    'type' => 'array',
-	  'current' => max( 1, get_query_var('paged') ),
-	  'total' => $wp_query->max_num_pages,
-	  'prev_text' => '&laquo;',
-	  'next_text' => '&raquo;',
-  ));
-
-  if(empty($links)){
-    return;
-  }
-
-  $links_html = '<ul class="pagination"><li>';
-  $links_html .= join('</li><li>', $links);
-  $links_html .= '</ul>';
-
-  $links_html = str_replace('<li><span class=\'page-numbers current\'>1</span>', '<li class="disabled"><span>&laquo;</span></li><li><span class=\'page-numbers current\'>1</span>', $links_html);
-  $links_html = str_replace('<li><span class="page-numbers dots">', '<li class="disabled"><span class=\'page-numbers dots\'>', $links_html);
-  $links_html = str_replace('<li><span class=\'page-numbers current\'>', '<li class="active"><span class=\'page-numbers current\'>', $links_html);
-
-  echo $links_html;
-}
-
-
-
 /* AJAX URL */
 
 add_action('wp_head', function() {
