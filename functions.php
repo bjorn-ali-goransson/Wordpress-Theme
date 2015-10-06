1024652bb41fd110b5dba89f60dbe32fc550e5dd
Björn Ali Göransson <bornemix@gmail.com>
2015-10-06 09:50:27 +0200

    Move get image functionality to function module

--- functions.php
+++ functions.php
@@ -122,30 +122,6 @@ add_action("after_switch_theme", function(){
 
 
 
-/* GET IMAGE FROM META */
-
-function get_image_from_meta($post_id, $meta_key, $size = 'full'){
-  $image_id = get_post_meta($post_id, $meta_key, TRUE);
-
-  return get_image($image_id, $size);
-}
-
-
-
-/* GET IMAGE */
-
-function get_image($image_id, $size = 'full'){
-  $image = wp_get_attachment_image_src($image_id, $size);
-
-  return (object)array(
-    'url' => $image[0],
-    'width' => $image[1],
-    'height' => $image[2],
-  );
-}
-
-
-
 /* OTHER */
 
 add_theme_support('automatic-feed-links');
