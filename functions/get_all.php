150f77c7b98edb28e0366044431cce4270be7597
Björn Ali Göransson <bornemix@gmail.com>
2015-10-09 18:07:49 +0200

    First implementation of get_all

---
+++ functions/get_all.php
@@ -0,0 +1,23 @@
+<?php
+  
+
+
+/* GET ALL */
+
+function get_all($post_type){
+  $post_objects = get_posts(array(
+    'post_type' => $post_type,
+    'nopaging' => TRUE,
+  ));
+
+  $posts = array();
+
+  foreach($post_objects as $post_object){
+    $posts[] = (object)array(
+      'id' => $post_object->ID,
+      'name' => $post_object->post_title,
+    );
+  }
+
+  return $posts;
+}
