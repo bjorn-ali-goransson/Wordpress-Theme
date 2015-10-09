6fa4004e0eabedc8e783b23c793b39c6a3edf531
Björn Ali Göransson <bornemix@gmail.com>
2015-10-09 16:40:06 +0200

    First implementation of Add post type function module

---
+++ functions/add_post_type.php
@@ -0,0 +1,46 @@
+<?php
+
+
+
+/* ADD POST TYPE */
+
+function add_post_type($arg1, $arg2){
+  if(!isset($GLOBALS['my_post_types'])){
+    $GLOBALS['my_post_types'] = array();
+  }
+
+  $GLOBALS['my_post_types'][] = get_post_type_object($arg1, $arg2);
+}
+
+function get_post_type_object($arg1, $arg2){
+  $id = $arg1;
+  $title = $arg2;
+
+  return array(
+    'id' => $id,
+    'options' => array(
+      'public' => FALSE,
+      'show_ui' => TRUE,
+      'show_in_nav_menus' => TRUE,
+      'labels' => array(
+        'name' => $title,
+      ),
+      'supports' => array('title'),
+      //'has_archive' => 'my_slug', // Don't forget to visit Permalinks after changing this
+    )
+  );
+}
+
+
+
+/* OTHER */
+
+add_action('init', function(){
+  if(!isset($GLOBALS['my_post_types'])){
+    return;
+  }
+
+  foreach($GLOBALS['my_post_types'] as $post_type_object){
+    register_post_type($post_type_object->id, $post_type_object->options);
+  }
+});
