63bb78dac47827946490af3a4e2087dde4c610ae
Björn Ali Göransson <bornemix@gmail.com>
2015-10-09 17:51:05 +0200

    First commit of string functions function module

---
+++ functions/string_functions.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+/* STRING BEGINS WITH */
+
+function string_begins_with($haystack, $needle) { // http://stackoverflow.com/a/10473026/3638471
+    return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
+}
+
+
+
+/* STRING ENDS WITH */
+
+function string_ends_with($haystack, $needle) { // http://stackoverflow.com/a/10473026/3638471
+    return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
+}
+
+
+
+/* TRIM OFF END OF STRING */
+
+function trim_off_end_of_string($string, $number_of_characters){
+  return substr($string, 0, -$number_of_characters);
+}
