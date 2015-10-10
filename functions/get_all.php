0a552725833a18760ff9d968e97ecd52f925c191
Björn Ali Göransson <bornemix@gmail.com>
2015-10-10 13:08:16 +0200

    Get all should order by title by default

--- functions/get_all.php
+++ functions/get_all.php
@@ -8,6 +8,8 @@ function get_all($post_type){
   $post_objects = get_posts(array(
     'post_type' => $post_type,
     'nopaging' => TRUE,
+    'orderby' => 'post_title',
+    'order' => 'ASC',
   ));
 
   $posts = array();
