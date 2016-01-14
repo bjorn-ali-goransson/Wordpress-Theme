bb569162bb75e558b271d415361941679626e09c
Björn Ali Göransson <bornemix@gmail.com>
2016-01-14 23:24:07 +0100

    Add .dom-loaded and .images-loaded to body

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,8 @@ var $window = $(window);
 var $body;
 
 $(function () { $body = $(document.body); });
+$(function () { $body.addClass('dom-loaded'); });
+$window.load(function () { $body.addClass('images-loaded'); });
 
 var app = angular.module('app', []);
 
