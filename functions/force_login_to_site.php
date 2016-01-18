7cc1c84c057802bb03ac430659725e0cab1b5c42
Björn Ali Göransson <bornemix@gmail.com>
2016-01-18 12:08:09 +0100

    Bring redirect_url and whitelist into closure

--- functions/force_login_to_site.php
+++ functions/force_login_to_site.php
@@ -7,7 +7,7 @@ require_once dirname(__FILE__) . '/get_current_url.php';
 /* FORCE LOGIN TO SITE */
 
 function force_login_to_site($redirect_url = NULL, $whitelist = array()){ // https://github.com/kevinvess/wp-force-login
-  add_action('init', function() {
+  add_action('init', function()use($redirect_url, $whitelist) {
     if(!is_user_logged_in()) {
       $url = get_current_url();
       
