b7b63f1a28fe7c0f83c9371688b7b355d16a03a4
Björn Ali Göransson <bornemix@gmail.com>
2016-03-06 15:04:20 +0100

    Add Bootstrap breadcrumbs() func module

---
+++ functions/breadcrumbs.php
@@ -0,0 +1,47 @@
+<?php
+function breadcrumbs(){
+  global $post;
+  ?>
+    <ol class="breadcrumb">
+      <?php
+        $ids = array(get_option('page_on_front'));
+
+        if(is_home()){
+            $page_for_posts = get_post(get_option('page_for_posts'));
+          
+            foreach($page_for_posts->ancestors as $id){
+              array_push($ids, $id);
+            }
+                      
+            array_push($ids, $page_for_posts->ID);
+        } else {
+          if($post->post_type == 'page'){
+            foreach(array_reverse($post->ancestors) as $id){
+              array_push($ids, $id);
+            }
+          }
+
+          if($post->post_type == 'post'){
+            $page_for_posts = get_post(get_option('page_for_posts'));
+          
+            foreach($page_for_posts->ancestors as $id){
+              array_push($ids, $id);
+            }
+                      
+            array_push($ids, $page_for_posts->ID);
+          }
+
+          array_push($ids, $post->ID);
+        }
+
+        foreach($ids as $i => $id){
+          ?>
+            <li<?= $id == array_slice($ids, -1)[0] ? ' class="active"' : '' ?>>
+              <a href="<?php echo get_permalink($id); ?>"><?php echo get_the_title($id); ?></a>
+            </li>
+          <?php
+        }
+      ?>
+    </ol>
+  <?php
+}
