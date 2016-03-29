57a17c8559b0d9f368ab1dbc913b99df686e5f36
Björn Ali Göransson <bornemix@gmail.com>
2016-03-29 18:36:25 +0200

    Also load files directly in application/

--- functions.php
+++ functions.php
@@ -18,6 +18,10 @@ require DIRNAME(__FILE__) . '/application.php';
 
 /* LOAD APPLICATION MODULES */
 
+foreach(glob(dirname(__FILE__) . '/application/*.php') as $filename){
+  require_once $filename;
+}
+
 foreach(glob(dirname(__FILE__) . '/application/**/*.php') as $filename){
   require_once $filename;
 }
