b2a87b85da2f0241b1a4583e29a92cf1d6a552d8
Björn Ali Göransson <bornemix@gmail.com>
2016-04-03 02:30:54 +0200

    Get single post function

---
+++ functions/getter_functions/get.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+/* GET */
+
+function get($id, $meta_key, $meta_value, $properties = array()){
+  $post_object = get_post($id);
+  
+  if($post_object == NULL){
+    return NULL;
+  }
+
+  $post = (object)array(
+    'id' => $post_object->ID,
+    'name' => $post_object->post_title,
+  );
+    
+  foreach($properties as $property){
+    $post->$property = get_post_meta($post->id, $property, TRUE);
+  }
+    
+  return $post;
+}
