0dbfced8eede34eb4005b4301d2fc1a3b741c0aa
Björn Ali Göransson <bornemix@gmail.com>
2016-04-26 13:31:30 +0200

    Split image func into separate files

--- functions/get_image.php
+++ functions/get_image.php
@@ -2,16 +2,6 @@
 
 
 
-/* GET IMAGE FROM META */
-
-function get_image_from_meta($post_id, $meta_key, $size = 'full'){
-  $image_id = get_post_meta($post_id, $meta_key, TRUE);
-
-  return get_image($image_id, $size);
-}
-
-
-
 /* GET IMAGE */
 
 function get_image($image_id, $size = 'full'){
---
+++ functions/get_image_from_meta.php
@@ -0,0 +1,11 @@
+<?php
+
+
+
+/* GET IMAGE FROM META */
+
+function get_image_from_meta($post_id, $meta_key, $size = 'full'){
+  $image_id = get_post_meta($post_id, $meta_key, TRUE);
+
+  return get_image($image_id, $size);
+}
