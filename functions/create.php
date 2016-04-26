2c7a93daaf21072b34f19c44c61569cdbbcae42d
Björn Ali Göransson <bornemix@gmail.com>
2016-04-26 12:42:52 +0200

    Add more post parameters

--- functions/create.php
+++ functions/create.php
@@ -13,9 +13,22 @@ function create($post_type, $post_title, $properties = array()){
     'post_type' => $post_type,
     'post_status' => 'publish',
     'post_title' => $post->name,
+    'post_content' => array_key_exists('post_content', $properties) ? $properties['post_content'] : NULL,
+    'post_date' => array_key_exists('post_date', $properties) ? $properties['post_date'] : NULL,
+    'post_date_gmt' => array_key_exists('post_date_gmt', $properties) ? $properties['post_date_gmt'] : NULL,
   ));
 
   foreach($properties as $key => $value){
+    if($key == 'post_content'){
+      continue;
+    }
+    if($key == 'post_date'){
+      continue;
+    }
+    if($key == 'post_date_gmt'){
+      continue;
+    }
+
     update_post_meta($post->id, $key, $value);
     $post->$key = $value;
   }
