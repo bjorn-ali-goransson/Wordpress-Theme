8da3093b28659b91b6389358ab2e270c86d1c0a5
Björn Ali Göransson <bornemix@gmail.com>
2016-04-26 13:31:42 +0200

    Make post thumbnail default meta key

--- functions/get_image_from_meta.php
+++ functions/get_image_from_meta.php
@@ -4,7 +4,7 @@
 
 /* GET IMAGE FROM META */
 
-function get_image_from_meta($post_id, $meta_key, $size = 'full'){
+function get_image_from_meta($post_id, $meta_key = '_thumbnail_id', $size = 'full'){
   $image_id = get_post_meta($post_id, $meta_key, TRUE);
 
   return get_image($image_id, $size);
