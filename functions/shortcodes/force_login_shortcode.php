4f742f76e583ae99c5258bb9829c40945ad9de01
Björn Ali Göransson <bornemix@gmail.com>
2016-04-27 15:36:10 +0200

    Change path due to file move

--- functions/shortcodes/force_login_shortcode.php
+++ functions/shortcodes/force_login_shortcode.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once dirname(__FILE__) . '/get_current_url.php';
+require_once dirname(__FILE__) . '/../get_current_url.php';
 require_once dirname(__FILE__) . '/noop_shortcode.php';
-require_once dirname(__FILE__) . '/if_post_contains_shortcode.php';
+require_once dirname(__FILE__) . '/../if_post_contains_shortcode.php';
   
 
 
