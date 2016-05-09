ea1a4ad279a95e101d28f35afbcd42e3bf415066
Björn Ali Göransson <bornemix@gmail.com>
2016-05-09 10:58:37 +0200

    Fix BR tag name

--- functions/shortcodes/br.php
+++ functions/shortcodes/br.php
@@ -6,4 +6,4 @@ require_once dirname(__FILE__) . '/../utility_functions/get_html_element.php';
 
 /* BR SHORTCODE */
 
-add_shortcode('br', function($attributes, $content){ return get_html_element('div', $attributes, $content); });
+add_shortcode('br', function($attributes, $content){ return get_html_element('br', $attributes, $content); });
