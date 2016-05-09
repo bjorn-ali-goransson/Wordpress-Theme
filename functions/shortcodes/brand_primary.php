4dc77acf3a2b697c8d1ccbe2f9db2110c7efeb19
Björn Ali Göransson <bornemix@gmail.com>
2016-05-09 13:06:42 +0200

    Allow for nested shortcodes

--- functions/shortcodes/brand_primary.php
+++ functions/shortcodes/brand_primary.php
@@ -6,4 +6,4 @@ require_once dirname(__FILE__) . '/../utility_functions/get_html_element.php';
 
 /* HR SHORTCODE */
 
-add_shortcode('brand_primary', function($attributes, $content){ return get_html_element('span', $attributes, $content, 'brand-primary'); });
+add_shortcode('brand_primary', function($attributes, $content){ return get_html_element('span', $attributes, do_shortcode($content), 'brand-primary'); });
