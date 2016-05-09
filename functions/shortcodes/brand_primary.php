92677b7abc693f339c6c83ade97fb554279c8dd3
Björn Ali Göransson <bornemix@gmail.com>
2016-05-09 13:07:42 +0200

    Correct comment heading

--- functions/shortcodes/brand_primary.php
+++ functions/shortcodes/brand_primary.php
@@ -4,6 +4,6 @@ require_once dirname(__FILE__) . '/../utility_functions/get_html_element.php';
 
 
 
-/* HR SHORTCODE */
+/* BRAND PRIMARY SHORTCODE */
 
 add_shortcode('brand_primary', function($attributes, $content){ return get_html_element('span', $attributes, do_shortcode($content), 'brand-primary'); });
