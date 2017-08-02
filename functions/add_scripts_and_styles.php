cf0df82881c5a999e477c8616501d3c2181ff4fe
Björn Ali Göransson <bornemix@gmail.com>
2017-08-02 13:07:27 +0200

    Fix cache busting after moving into functions/

--- functions/add_scripts_and_styles.php
+++ functions/add_scripts_and_styles.php
@@ -139,7 +139,7 @@ function get_my_script_or_style_object($name, $dependencies = ''){
     'id' => (empty($vendor) ? '' : $vendor . '/') . $name,
     'url' => get_template_directory_uri() . $path,
     'dependencies' => $dependencies,
-    'version' => strpos($_SERVER["HTTP_HOST"], 'localhost') === FALSE ? @filemtime(dirname(__FILE__) . $path) : NULL,
+    'version' => strpos($_SERVER["HTTP_HOST"], 'localhost') === FALSE ? @filemtime(dirname(__FILE__) . '/..' . $path) : NULL,
   );
 }
 
