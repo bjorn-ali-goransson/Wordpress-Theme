e0ad34d9f4019732d0df66fb0f72717db2a1d934
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2017-11-05 00:30:52 +0100

    Enable overriding of options

--- functions/add_post_type.php
+++ functions/add_post_type.php
@@ -19,18 +19,22 @@ function add_post_type($id, $label, $options = array()){
 function get_my_post_type_object($id, $label, $options = array()){
   $supports = array_merge(array('title'), !empty($options['supports']) ? $options['supports'] : array());
 
+  $default_options = array(
+    'public' => FALSE,
+    'show_ui' => TRUE,
+    'show_in_nav_menus' => TRUE,
+    'labels' => array(
+      'name' => $label,
+    ),
+    'supports' => $supports,
+    //'has_archive' => 'my_slug', // Don't forget to visit Permalinks after changing this
+  );
+
+  $options = array_merge($default_options, $options);
+
   return (object)array(
     'id' => $id,
-    'options' => array(
-      'public' => FALSE,
-      'show_ui' => TRUE,
-      'show_in_nav_menus' => TRUE,
-      'labels' => array(
-        'name' => $label,
-      ),
-      'supports' => $supports,
-      //'has_archive' => 'my_slug', // Don't forget to visit Permalinks after changing this
-    )
+    'options' => $options,
   );
 }
 
