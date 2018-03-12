c2f4302ad232e094637beebf7b8b02f832932b41
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2018-03-12 01:01:01 +0100

    Remove twbs markup

--- header.php
+++ header.php
@@ -11,25 +11,5 @@
   </head>
 <body ng-controller="Main" <?php body_class('no-js'); ?>><script>document.body.className = document.body.className.replace(" no-js", " js");</script>
   <!--[if lt IE 9]>
-    <div class="alert alert-warning"><div class="container">You are using an outdated browser. <a href="http://browsehappy.com/" class="alert-link">Upgrade your browser today</a></div></div>
+    <p>You are using an outdated browser. <a href="http://browsehappy.com/" class="alert-link">Upgrade your browser today</a></p>
   <![endif]-->
-
-  <div class="content-wrapper">
-    <nav class="navbar" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only"><?php _e('Toggle navigation'); ?></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          
-          <a class="navbar-brand" href="<?php echo site_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png"></a>
-        </div>
-
-        <div class="collapse navbar-collapse">
-          <?php my_menu('main', "nav navbar-nav") ?>
-        </div>
-      </div>
-    </nav>
