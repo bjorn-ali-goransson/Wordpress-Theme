b30d4ed9eb0dec637b49ff3b6646c4d1b60ccd58
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2018-06-12 06:40:45 +0200

    Add support for getting permalinks

--- functions/orm_functionality/post/load_properties.php
+++ functions/orm_functionality/post/load_properties.php
@@ -9,8 +9,16 @@ function load_properties($post_object, $properties){
     'id' => $post_object->ID,
     'name' => $post_object->post_title,
   );
+
+  if(in_array('permalink', $properties)){
+    $post->permalink = get_permalink($post_object->ID);
+  }
   
   foreach($properties as $property){
+    if(isset($post->$property)){
+      continue;
+    }
+
     if(strpos($property, 'post_') === 0){
       $post->$property = $post_object->$property;
     } else {
