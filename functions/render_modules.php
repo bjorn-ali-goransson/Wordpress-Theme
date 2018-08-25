a710a647475654ca5e10116f5e4059ca32416358
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2018-08-25 03:26:16 +0200

    Add auto generation of modules, responsive images

--- functions/render_modules.php
+++ functions/render_modules.php
@@ -9,7 +9,8 @@ function render_modules(){
   
   if(!empty($modules)){
     foreach($modules as $module){
-      $function_name = 'module_' . $module['acf_fc_layout'];
+      $name = $module['acf_fc_layout'];
+      $function_name = 'module_' . $name;
       
       if(function_exists($function_name)){
         $function_name($module);
@@ -17,6 +18,56 @@ function render_modules(){
         ?>
           <p>Could not display <?= $function_name ?></p>
         <?php
+
+        if(user_has_role('administrator')){
+          $path = get_template_directory() . '/application/modules';
+          $src = $path . '/' . $name . '.php';
+          
+          if(file_exists($src)){
+            continue;
+          }
+          
+          if(!file_exists($path)){
+            mkdir($path);
+          }
+
+          $contents = '';
+
+          $contents .= "<?php\n";
+          $contents .= "\n";
+          $contents .= "\n";
+          $contents .= "/* " . strtoupper(str_replace('_', ' ', $name)) . " */\n";
+          $contents .= "\n";
+          $contents .= "function $function_name(\$module){\n";
+          $contents .= "  ?>\n";
+          
+          foreach($module as $key => $value) {
+            if($key == 'acf_fc_layout') {
+              continue;
+            }
+
+            if(+$value > 0) {
+              $contents .= "    <div class=\"image\" responsive-background-image>\n";
+              $contents .= "      <img class=\"responsive-background-image\" src=\"<?= get_src(\$module['$key']) ?>\" srcset=\"<?= get_srcset(\$module['$key']) ?>\" alt=\"\">\n";
+              $contents .= "    </div>\n";
+              
+              continue;
+            }
+
+            $tag = 'div';
+
+            if($key == 'heading'){
+              $tag = 'h2';
+            }
+
+            $contents .= "    <$tag><?= \$module['$key'] ?></$tag>\n";
+          }
+
+          $contents .= "  <?php\n";
+          $contents .= "}\n";
+
+          file_put_contents($src, $contents);
+        }
       }
     }
   }
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,30 @@
 var $ = jQuery;
+
+
+
+/* RESPONSIVE BACKGROUND IMAGE */
+
+$(function () {
+  $('[responsive-background-image]').each(function () {
+    var element = $(this);
+    var image = element.children('.responsive-background-image');
+
+    if (!image.length) {
+      return;
+    }
+
+    var src = null;
+    var update = throttle(function () {
+      var newSrc = image[0].currentSrc ? image[0].currentSrc : image[0].src;
+
+      if (src != newSrc) {
+        src = newSrc;
+        element.css({ backgroundImage: 'url("' + src + '")' });
+      }
+    }, 100);
+
+    image.load(update);
+    setTimeout(update, 1000);
+    $(window).resize(update);
+  });
+});
