0abeb495fe379d15fd27f817722ed75f09eeec98
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2018-12-03 23:23:31 +0100

    Add support for nested menu items

--- functions/get_menu_items.php
+++ functions/get_menu_items.php
@@ -13,5 +13,31 @@ function get_menu_items($menu_name){
 
   $menu = wp_get_nav_menu_object($locations[$menu_name]);
 
-  return $menu_items = wp_get_nav_menu_items($menu->term_id);
+  $menu_items = wp_get_nav_menu_items($menu->term_id);
+
+  $children = array();
+
+  foreach($menu_items as $menu_item){
+    $menu_item->children = array();
+
+    $children[$menu_item->ID] = &$menu_item->children;
+  }
+
+  foreach($menu_items as $menu_item){
+    if(!$menu_item->menu_item_parent){
+      continue;
+    }
+
+    $children[$menu_item->menu_item_parent][] = $menu_item;
+  }
+
+  foreach($menu_items as $i => $menu_item){
+    if(!$menu_item->menu_item_parent){
+      continue;
+    }
+
+    unset($menu_items[$i]);
+  }
+
+  return $menu_items;
 }
