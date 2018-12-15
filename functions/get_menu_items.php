6451b5110dac036418e44efd6984b708c9124e2e
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2018-12-15 22:51:48 +0100

    Add open state to menu items

--- functions/get_menu_items.php
+++ functions/get_menu_items.php
@@ -5,6 +5,8 @@
 /* GET MENU ITEMS */
 
 function get_menu_items($menu_name){
+  global $wp_query;
+
   $locations = get_nav_menu_locations();
 
   if(!$locations || !isset($locations[$menu_name])){
@@ -14,6 +16,18 @@ function get_menu_items($menu_name){
   $menu = wp_get_nav_menu_object($locations[$menu_name]);
 
   $menu_items = wp_get_nav_menu_items($menu->term_id);
+
+  $current_page_ancestors = get_post_ancestors($wp_query->queried_object_id);
+
+  foreach($menu_items as $menu_item){
+    if(!$menu_item->object_id){
+      $menu_item->active = FALSE;
+      continue;
+    }
+    
+    $menu_item->active = $wp_query->queried_object_id == $menu_item->object_id;
+    $menu_item->open = $wp_query->queried_object_id == $menu_item->object_id || in_array($menu_item->object_id, $current_page_ancestors);
+  }
 
   $children = array();
 
