f8c4bc8131a331c97655f9568645c1516715d0d1
Björn Göransson <bjorn.a.goransson@gmail.com>
2019-09-11 21:45:23 +0200

    Respect page order

--- functions/get_children_of.php
+++ functions/get_children_of.php
@@ -7,7 +7,7 @@
 function get_children_of($id){
   $result = array();
 
-  foreach(get_posts(array('numberposts' => -1, 'post_status' => 'publish', 'post_type' => 'page', 'post_parent' => $id, 'suppress_filters' => false)) as $child){
+  foreach(get_posts(array('numberposts' => -1, 'post_status' => 'publish', 'post_type' => 'page', 'post_parent' => $id, 'suppress_filters' => false, 'sort_column' => 'menu_order', 'sort_order' => 'asc')) as $child){
     $result[] = (object)(array(
       'post' => $child,
       'children' => get_children_of($child->ID)
