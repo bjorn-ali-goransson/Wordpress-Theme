2156b9b40cbfd0ebad64ec3b7b4b8d955db84a69
Björn Göransson <bjorn.a.goransson@gmail.com>
2019-09-14 09:52:11 +0200

    Add get_page_with_template()

---
+++ functions/get_page_with_template.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+/* GET PAGE WITH TEMPLATE */
+
+function get_page_with_template($template_name){
+  $pages = get_posts(array(
+    'post_type' => 'page',
+    'numposts' => 1,
+    'meta_key' => '_wp_page_template',
+    'meta_value' => "{$template_name}.php",
+  ));
+  
+  if(empty($pages)){
+    return null;
+  }
+
+  return $pages[0];
+}
