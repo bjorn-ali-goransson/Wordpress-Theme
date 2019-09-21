91d1ef0ba22d53e437058f422b59aa9079c23593
Björn Göransson <bjorn.a.goransson@gmail.com>
2019-09-21 12:32:28 +0200

    Limit number of posts, fix typo

--- functions/get_page_with_template.php
+++ functions/get_page_with_template.php
@@ -7,7 +7,7 @@
 function get_page_with_template($template_name){
   $pages = get_posts(array(
     'post_type' => 'page',
-    'numposts' => 1,
+    'numberposts' => 1,
     'meta_key' => '_wp_page_template',
     'meta_value' => "{$template_name}.php",
   ));
